6582659f03597bd336c8fa8d5944ce4989395163
OhtoAi1024 <muzhi567@qq.com>
2024-03-21 21:28:38 +0800

    feat: 实现验证码发送功能

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,16 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -14,7 +20,25 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 校验手机号
+        if (null == phone || RegexUtils.isPhoneInvalid(phone)) {
+            // 如果不符合, 返回错误信息
+            return Result.fail("手机号格式错误!");
+        }
+
+        // 符合, 生成验证码
+        String code = RandomUtil.randomNumbers(6);
+        // 保存验证码到session
+        session.setAttribute("code", code);
+        // 发送验证码
+        log.debug("发送验证码成功, 验证码{}", code);
+        // 返回ok
+        return Result.ok();
+    }
 }
